0c135083449683f50d067a8d3de41c2050d4bbec
UDAY KUMAR G <144527336+UDAY2031@users.noreply.github.com>
2024-11-08 16:22:29 +0530

    Updated portfolio with new changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,18 +17,29 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/uday-kumar-g-3956312a4/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://github.com/UDAY2031"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/geek_2031?igsh=YjhscHI4aWVud294"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/uday-kumar-g-3956312a4/">
+                <img src={navIcon1} alt="LinkedIn" />
+              </a>
+              <a href="https://github.com/UDAY2031">
+                <img src={navIcon2} alt="GitHub" />
+              </a>
+              <a href="https://www.instagram.com/geek_2031?igsh=YjhscHI4aWVud294">
+                <img src={navIcon3} alt="Instagram" />
+              </a>
             </div>
-            {/* Add CV download button here */}
+            {/* Add some space between social icons and CV download */}
+            <div className="social-space"></div>  
+            
+            {/* Add CV download button */}
             <div className="cv-download">
-              <a href={cvFile} download className="btn btn-primary">Download CV</a>
+              <a href={cvFile} download className="btn btn-primary cv-btn">
+                Download CV
+              </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
